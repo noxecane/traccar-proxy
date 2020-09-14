b1d082b5ce13d6f41e9f24df04d0150de7d1b552
Olakunle Arewa <arewa.olakunle@gmail.com>
2020-09-14 14:17:17 +0100

    feat: added endpoint for querying all positions

--- pkg/http/rest/positions.go
+++ pkg/http/rest/positions.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/go-chi/chi"
 	"github.com/tsaron/anansi"
+	"tsaron.com/positions/pkg/model"
 	"tsaron.com/positions/pkg/traccar"
 )
 
@@ -22,8 +23,59 @@ type positionQuery struct {
 
 func Positions(r *chi.Mux, repo *traccar.Repo) {
 	r.Route("/positions", func(r chi.Router) {
+		r.Get("/", getPositions(repo))
 		r.Get("/latest", getLatestPosition(repo))
 	})
+}
+
+func getPositions(repo *traccar.Repo) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		q := new(positionQuery)
+		anansi.ReadQuery(r, q)
+
+		if q.Device == 0 {
+			panic(anansi.APIError{
+				Code:    http.StatusBadRequest,
+				Message: "You need to pass a device ID",
+			})
+		}
+
+		if q.From.IsZero() != q.To.IsZero() {
+			panic(anansi.APIError{
+				Code:    http.StatusBadRequest,
+				Message: "Both from and to must be set if any is set at all",
+			})
+		}
+
+		var tps []model.TraccarPosition
+		var err error
+
+		if q.From.IsZero() {
+			tps, err = repo.Positions(r.Context(), q.Device, q.Limit)
+		} else {
+			tps, err = repo.PositionsBetween(r.Context(), q.Device, q.From.UTC(), q.To.UTC())
+		}
+
+		if err != nil {
+			panic(err)
+		}
+
+		var ps []model.Position
+		for _, tp := range tps {
+			p, err := traccar.TransformPosition(tp)
+			if err != nil {
+				panic(anansi.APIError{
+					Code:    http.StatusUnprocessableEntity,
+					Message: "Could not parse position because of attribute",
+					Err:     err,
+					Meta:    p, // send this as it's still useful
+				})
+			}
+			ps = append(ps, p)
+		}
+
+		anansi.SendSuccess(r, w, ps)
+	}
 }
 
 func getLatestPosition(repo *traccar.Repo) http.HandlerFunc {
